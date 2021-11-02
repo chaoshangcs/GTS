e71b3e1a541d07efb04e096070d406707803def7
moghadas76 <s.m.moghadas2012@gmail.com>
2021-11-02 21:11:58 +0330

    Fix

--- model/pytorch/supervisor.py
+++ model/pytorch/supervisor.py
@@ -247,7 +247,7 @@ class GTSSupervisor:
 
 
     def _train(self, base_lr,
-               steps, patience=200, epochs=100, lr_decay_ratio=0.1, log_every=1, save_model=0,
+               steps, patience=200, epochs=100, lr_decay_ratio=0.1, log_every=1, save_model=1,
                test_every_n_epochs=10, epsilon=1e-8, **kwargs):
         # steps is used in learning rate - will see if need to use it?
         min_val_loss = float('inf')
