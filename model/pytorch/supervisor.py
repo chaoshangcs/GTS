7f20339f001568ef6e46226ef781784a0471afc6
moghadas76 <s.m.moghadas2012@gmail.com>
2021-11-11 10:44:09 +0330

    Fix colab problem

--- model/pytorch/supervisor.py
+++ model/pytorch/supervisor.py
@@ -131,7 +131,7 @@ class GTSSupervisor:
 
             for _, (x, y) in enumerate(val_iterator):
                 x, y = self._prepare_data(x, y)
-                output = self.GTS_mode(x, self._train_feas)
+                output = self.GTS_model(x, self._train_feas)
                 break
 
     def train(self, **kwargs):
